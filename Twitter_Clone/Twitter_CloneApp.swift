29e8d2fbdf33f6939377f9b6f4eb263bf888b2b2
Jinu Joy <jinuparekatil@gmail.com>
2024-06-13 18:59:26 +0530

    Initial Commit

---
+++ Twitter_Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
