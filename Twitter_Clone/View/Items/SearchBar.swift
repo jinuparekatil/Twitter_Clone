80c85ed4675ceb95d1e2300537969d515180b5f5
Jinu Joy <jinuparekatil@gmail.com>
2024-06-14 18:19:47 +0530

    Twitter UI - Search View 1

---
+++ Twitter_Clone/View/Items/SearchBar.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBar.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 14/06/2024.
+//
+
+import Foundation
---
+++ Twitter_Clone/View/Search/Search Cell.swift
@@ -0,0 +1,18 @@
+//
+//  Search Cell.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 14/06/2024.
+//
+
+import SwiftUI
+
+struct Search_Cell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Search_Cell()
+}
