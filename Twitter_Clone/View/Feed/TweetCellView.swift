59fd620159ca5ab41271c315c03153bfb76b68a0
Jinu Joy <jinuparekatil@gmail.com>
2024-06-14 15:37:45 +0530

    Twitter UI- FeedView

---
+++ Twitter_Clone/View/Feed/TweetCellView.swift
@@ -0,0 +1,18 @@
+//
+//  TweetCellView.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 14/06/2024.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TweetCellView()
+}
