b2ff43eaf927f75612e8de5df6dd758d2885ac7e
Jinu Joy <jinuparekatil@gmail.com>
2024-06-15 14:06:54 +0530

    TwitterUI: TopBar

---
+++ Twitter_Clone/View/Main/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 15/06/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack{
+            TopBar()
+            Home()
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Twitter_Clone/View/Main/TopBar.swift
@@ -0,0 +1,45 @@
+//
+//  TopBar.swift
+//  Twitter_Clone
+//
+//  Created by Jinu on 15/06/2024.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    @State var width = UIScreen.main.bounds.width
+    var body: some View {
+        VStack {
+            HStack{
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "line.horizontal.3")
+                        .font(.system(size: 24))
+                        .foregroundStyle(Color("bg"))
+                })
+                Spacer(minLength: 0)
+                
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding()
+                    .frame(width: 50,height: 50)
+                Spacer(minLength: 0)
+            }
+            .padding()
+            
+            Rectangle()
+                .frame(width: width,height: 1)
+                .foregroundStyle(.gray
+                    .opacity(0.3))
+            
+        }
+        .background(Color.white)
+    }
+}
+
+#Preview {
+    TopBar()
+}
