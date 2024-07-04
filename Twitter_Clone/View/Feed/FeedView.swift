e1614a7990e7f8c892ffb3d8e820087cab2214b6
Jinu Joy <jinuparekatil@gmail.com>
2024-07-04 13:12:30 +0530

    pull to refrash the scroll view

--- Twitter_Clone/View/Feed/FeedView.swift
+++ Twitter_Clone/View/Feed/FeedView.swift
@@ -13,18 +13,25 @@ struct FeedView: View {
     let user: User
     
     var body: some View {
-        ScrollView(.vertical,showsIndicators: false) {
-            LazyVStack(spacing: 18, content: {
-                ForEach(viewModel.tweets) { tweet in
-                    
-                    TweetCellView(viewModel: TweetCellViewModel(tweet: tweet, currentUser: user))
-                    
-                    Divider()
-                }
-            })
-            .padding(.top)
-            .padding(.horizontal)
-            .zIndex(0)
+        RefreshableScrollView(content:
+            ScrollView(.vertical,showsIndicators: false) {
+                LazyVStack(spacing: 18, content: {
+                    ForEach(viewModel.tweets) { tweet in
+                        
+                        TweetCellView(viewModel: TweetCellViewModel(tweet: tweet, currentUser: user))
+                        
+                        Divider()
+                    }
+                })
+                .padding(.top)
+                .padding(.horizontal)
+                .zIndex(0)
+            }
+        ) { control in
+            DispatchQueue.main.async {
+                self.viewModel.fetchTweets()
+                control.endRefreshing()
+            }
         }
     }
 }
